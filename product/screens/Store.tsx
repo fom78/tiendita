d4409c473625e757a0e7e441934c780b71b94d41
fom78 <fom78a@gmail.com>
2021-12-27 12:11:37 -0300

    iniciando carrito

--- product/screens/Store.tsx
+++ product/screens/Store.tsx
@@ -1,5 +1,14 @@
 import * as React from "react";
-import { Button, Flex, Grid, Image, Link, Stack, Text } from "@chakra-ui/react";
+import { Button, Flex, Grid, Image, Link, Stack, Text,
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton, 
+  useDisclosure,
+  Input} from "@chakra-ui/react";
 
 import { Product } from "../../product/types";
 import { parseCurrency } from "../../utils/currency";
@@ -27,32 +36,73 @@ const StoreScreen: React.FC<Props> = ({ products }) => {
     [cart],
   );
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const btnRef = React.useRef()
+
+
   return (
-    <Stack spacing={6}>
-      {Boolean(products.length)?
-      <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
-      {products.map((product) => (
-        <ProductCard 
-          product={product} 
-          key={product.id} 
-          onAdd={(product) => setCart((cart) => cart.concat(product))}/>
-      ))}
-    </Grid>
-    : <Text color="gray.500" fontSize="lg" margin="auto">No hay productos!!</Text>}
-      {Boolean(cart.length) && (
-        <Flex alignItems="center" bottom={4} justifyContent="center" position="sticky">
-          <Button
-            isExternal
-            as={Link}
-            colorScheme="whatsapp"
-            href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(text)}`}
-            width="fit-content"
-          >
-            Completar pedido ({cart.length} productos)
-          </Button>
-        </Flex>
-      )}
-    </Stack>
+    <>
+      <Stack spacing={6}>
+        {Boolean(products.length) ?
+          <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
+            {products.map((product) => (
+              <ProductCard
+                product={product}
+                key={product.id}
+                onAdd={(product) => setCart((cart) => cart.concat(product))} />
+            ))}
+          </Grid>
+          : <Text color="gray.500" fontSize="lg" margin="auto">No hay productos!!</Text>}
+        {Boolean(cart.length) && (
+          <Flex alignItems="center" bottom={4} justifyContent="center" position="sticky">
+            <Button
+              isExternal
+              colorScheme="whatsapp"
+              href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(text)}`}
+              width="fit-content"
+              onClick={onOpen}
+            >
+              Ver Carrito
+            </Button>
+          </Flex>
+        )}
+      </Stack>
+      <Button ref={btnRef} colorScheme='blue' onClick={onOpen}>
+        Open
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement='right'
+        onClose={onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Create your account</DrawerHeader>
+
+          <DrawerBody>
+            <Input placeholder='Type here...' />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Button variant='outline' mr={3} onClick={onClose}>
+              Limpiar
+            </Button>
+            <Button
+              isExternal
+              as={Link}
+              colorScheme="whatsapp"
+              href={`https://wa.me/${phoneNumber}?text=${encodeURIComponent(text)}`}
+              width="100%"
+              size="md"
+            >
+              Completar pedido ({cart.length} productos)
+            </Button>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </>
   );
 };
 
